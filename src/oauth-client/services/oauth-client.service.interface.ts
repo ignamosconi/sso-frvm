2b0e2179e4133c6cc760a45ed32384c7d37bad7e
Ignacio <ignamosconi@gmail.com>
2026-07-14 18:33:02 -0300

    bloque 5: hashear client secrets con bcrypt

--- src/oauth-client/services/oauth-client.service.interface.ts
+++ src/oauth-client/services/oauth-client.service.interface.ts
@@ -1,6 +1,7 @@
 import { CreateOAuthClientDto } from '../dtos/create-oauth-client.dto.js';
 import { UpdateOAuthClientDto } from '../dtos/update-oauth-client.dto.js';
 import { OAuthClientResponseDto } from '../dtos/oauth-client-response.dto.js';
+import { OAuthClientCreatedResponseDto } from '../dtos/oauth-client-created-response.dto.js';
 import { OAuthClientInfoDto } from '../dtos/oauth-client-info.dto.js';
 
 export interface IOAuthClientService {
@@ -8,9 +9,9 @@ export interface IOAuthClientService {
   findOne(id: number): Promise<OAuthClientResponseDto>;
   findInfo(id: number): Promise<OAuthClientInfoDto>;
   validateClient(id: number, secret: string, redirectUri: string): Promise<boolean>;
-  create(dto: CreateOAuthClientDto): Promise<OAuthClientResponseDto>;
+  create(dto: CreateOAuthClientDto): Promise<OAuthClientCreatedResponseDto>;
   update(id: number, dto: UpdateOAuthClientDto): Promise<OAuthClientResponseDto>;
   remove(id: number): Promise<void>;
-  regenerateSecret(id: number): Promise<OAuthClientResponseDto>;
+  regenerateSecret(id: number): Promise<OAuthClientCreatedResponseDto>;
   sendCredentialsByEmail(id: number, to: string): Promise<void>;
 }
